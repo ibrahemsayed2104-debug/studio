13a02ad3dba610d2a7ba2739a4ae5bf3dc5a60e0
Ibrahim Sayed <ibrahemsayed2104@gmail.com>
2026-01-06 20:53:10 +0000

    هات انت صور و حطها

--- src/lib/gallery-images.ts
+++ src/lib/gallery-images.ts
@@ -9,14 +9,14 @@ export type GalleryItem = {
 
 export const galleryImages: { [key: string]: GalleryItem[] } = {
   gallery: [
-    { id: 'new-1', url: 'https://m.media-amazon.com/images/I/71u9S+tRrJL._AC_SL1500_.jpg', hint: 'velvet curtain', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-2', url: 'https://m.media-amazon.com/images/I/61kGFFfbd2L._AC_SL1500_.jpg', hint: 'textured curtain', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-3', url: 'https://m.media-amazon.com/images/I/71fD+sXzJQL._AC_SL1500_.jpg', hint: 'grommet curtain', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-4', url: 'https://m.media-amazon.com/images/I/71d1xDw9rJL._AC_SL1500_.jpg', hint: 'dark curtain', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-5', url: 'https://m.media-amazon.com/images/I/61M6K5J2f4L._AC_SL1500_.jpg', hint: 'patterned curtain', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-6', url: 'https://www.ef-curtain.com/wp-content/uploads/2021/04/%D8%B3%D8%AA%D8%A7%D8%A6%D8%B1-%D8%B1%D9%88%D9%84-eichholtz-2.jpg', hint: 'roll curtain', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-7', url: 'https://i.postimg.cc/135X7RNH/2023-42.jpg', hint: 'modern drapes', type: 'image', aspectRatio: '1/1' },
-    { id: 'new-8', url: 'https://eg.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/09/802874/1.jpg?1487', hint: 'linen curtain', type: 'image', aspectRatio: '1/1' }
+    { id: 'gallery-1', url: 'https://images.unsplash.com/photo-1615875617371-6c434c568525?q=80&w=1932', hint: 'beige curtain', type: 'image', aspectRatio: '4/5' },
+    { id: 'gallery-2', url: 'https://images.unsplash.com/photo-1595265010264-c15786ca6c5a?q=80&w=1974', hint: 'burgundy curtain', type: 'image', aspectRatio: '2/3' },
+    { id: 'gallery-3', url: 'https://images.unsplash.com/photo-1601758124578-82559a4631fb?q=80&w=1974', hint: 'green curtain', type: 'image', aspectRatio: '3/4' },
+    { id: 'gallery-4', url: 'https://images.unsplash.com/photo-1533227475286-fecb4b4f9a48?q=80&w=2070', hint: 'grey curtain', type: 'image', aspectRatio: '2/3' },
+    { id: 'gallery-5', url: 'https://images.unsplash.com/photo-1524758631624-e2822e304c36?q=80&w=2070', hint: 'white interior', type: 'image', aspectRatio: '3/4' },
+    { id: 'gallery-6', url: 'https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2070', hint: 'luxury livingroom', type: 'image', aspectRatio: '2/3' },
+    { id: 'gallery-7', url: 'https://images.unsplash.com/photo-1631679704834-315967d7d860?q=80&w=1974', hint: 'modern drapes', type: 'image', aspectRatio: '3/4' },
+    { id: 'gallery-8', url: 'https://images.unsplash.com/photo-1578683010236-d716f9a3f458?q=80&w=1974', hint: 'bedroom curtain', type: 'image', aspectRatio: '2/3' }
   ],
   videos: [
       { id: 'vi-1', url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', hint: 'curtain showcase', type: 'video', aspectRatio: '16/9' },
